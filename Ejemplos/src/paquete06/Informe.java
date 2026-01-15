6b91617f2a62c4b2df0a8369caef67ed4567cd2a
daangamarca2 <arturoangamarca120@gmail.com>
2026-01-15 11:16:46 -0500

    Informe

---
+++ Ejemplos/src/paquete06/Informe.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package paquete06;
+
+/**
+ *
+ * @author A S U S A16
+ */
+public interface Informe {
+
+    public static void imprimir(int a, int b, int c, int d) {
+        System.out.printf("La suma de %d + %d es igual a: %d\n"
+                + "La multiplicacion de %d * %d es igual a: %d\n",
+                a, b,
+                c,
+                a, b,
+                d);
+    }
+}
